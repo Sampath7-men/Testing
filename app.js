5c612a48b1b83ef3206390a94da3823a951e7e3b
Sampath7-men <pindrasampathvarma@gmail.com>
2026-06-24 16:40:35 +0530

    project update app.js

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/plain" });
 
-```
-res.end(
-    "🚀 Deployment Test Successful!\n\n" +
+  res.end(
+    "Deployment Test Successful!\n\n" +
     "Server is running on AWS EC2\n" +
     "This is a simple Node.js app for manual deployment practice\n"
-);
-```
-
+  );
 });
 
 const PORT = 3000;
 
 server.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
